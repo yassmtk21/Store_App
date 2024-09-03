d8ea35cce1ce353ae779c0e25cb9cb9386f0edd0
yassmtk21 <yassinmoutik20@gmail.com>
2024-09-03 19:40:49 +0100

    Add cart.js

--- models/cart.js
+++ models/cart.js
@@ -10,8 +10,30 @@ const p = path.join(
 module.exports = class Cart {
   static addProduct(id, productPrice) {
     fs.readFile(p, (err, fileContent) => {
+      let cart = { products: [], totalPrice };
       if (!err) {
+        cart = JSON.parse(fileContent);
       }
+      const existingproductIndex = cart.products.findIndex(
+        (prod) => prod.id === id
+      );
+      const existingProduct = cart.products[existingproductIndex];
+      let updatedProduct;
+      if (existingProduct) {
+        updatedProduct = { ...existingProduct };
+        updatedProduct.qty = updatedProduct.qty + 1;
+
+        // immulabilty concept using spread operator
+        cart.products = [...cart.products];
+        cart.products[existingproductIndex] = updatedProduct;
+      } else {
+        updatedProduct = { id: id, qty: 1 };
+        cart.products = [...cart.products, updatedProduct];
+      }
+      cart.totalPrice = cart.totalPrice + +productPrice;
+      fs.writeFile(p, JSON.stringify(cart), (err) => {
+        console.log(err);
+      });
     });
   }
 };
